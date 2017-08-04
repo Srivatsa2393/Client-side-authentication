e1424f9426f0c579b00d72f2e86f7a368d2bdf65
Srivatsa S <srivatsashankaran@gmail.com>
2017-08-04 16:15:13 +0530

    Adding the signin form with react-forms 5.3.3

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -2,20 +2,28 @@ import React, { Component } from 'react';
 import { reduxForm }from 'redux-form';
 
 class Signin extends Component{
+    handleFormSubmit({ email, password}) {
+        console.log(email, password);
+        //Need to do something to log user in
+    }
+
     render() {
+        //handleSubmit comes from redux forms
+        const { handleSubmit, fields: { email, password }} = this.props;
+
         return(
-            <form>
+            <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
                 <fieldset className="form-group">
                     <label>
                         Email:
                     </label>
-                    <input className="form-control" /> 
+                    <input {...email} className="form-control" /> 
                 </fieldset>
                  <fieldset className="form-group">
                     <label>
                         Password:
                     </label>
-                    <input className="form-control" /> 
+                    <input {...password} className="form-control" /> 
                 </fieldset>
                 <button action="submit" className="btn btn-primary">Sign in</button>
             </form>
@@ -26,4 +34,5 @@ class Signin extends Component{
 export default reduxForm({
     form: 'signin',
     fields: ['email', 'password']
-})(Signin);
+})(Signin); 
+
