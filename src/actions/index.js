a9fe0862c930a7a48f867f1aa670812493d85565
Srivatsa S <srivatsashankaran@gmail.com>
2017-08-04 20:26:59 +0530

    Introducing redux thunk into our actions aand action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,18 @@
+
+
+
 export function signInUser({ email, password }){
-  //Submit email password to server
+  return function(dispatch) {
+    //Submit email password to server
 
-  //If request is good..
-  //update a state to indicate the user is authenticated
-  //Save the JWT token
-  //redirect to the route '/feature'
+    //If request is good..
+    //update a state to indicate the user is authenticated
+    //Save the JWT token
+    //redirect to the route '/feature'
 
 
 
-  //if request is bad ..
-  //Show an error to the user
-
+    //if request is bad ..
+    //Show an error to the user
+  }
 }
