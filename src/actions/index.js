64b2796fe0c796797f03b840d30f96e8ba78ad4f
Srivatsa S <srivatsashankaran@gmail.com>
2017-08-04 19:02:06 +0530

    Creating an action creator which has many responsibilities

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+export function signInUser({ email, password }){
+  //Submit email password to server
+
+  //If request is good..
+  //update a state to indicate the user is authenticated
+  //Save the JWT token
+  //redirect to the route '/feature'
+
+
+
+  //if request is bad ..
+  //Show an error to the user
+
+}
