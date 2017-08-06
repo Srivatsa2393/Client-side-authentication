3b53d1a3987ceb5b1104f5c8cc2bbade67a45e7d
Srivatsa S <srivatsashankaran@gmail.com>
2017-08-06 20:00:04 +0530

    Programmatic navigation using react-router

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
 
 const ROOT_URL = 'http://localhost:3090';
 
@@ -6,16 +7,18 @@ const ROOT_URL = 'http://localhost:3090';
 export function signInUser({ email, password }){
   return function(dispatch) {
     //Submit email password to server
-    axios.post(`${ROOT_URL}/signin`, { email, password });
-
-    //If request is good..
-    //update a state to indicate the user is authenticated
-    //Save the JWT token
-    //redirect to the route '/feature'
-
-
-
-    //if request is bad ..
-    //Show an error to the user
+    axios.post(`${ROOT_URL}/signin`, { email, password })
+      .then(response => {
+        //If request is good..
+        //update a state to indicate the user is authenticated
+        //Save the JWT token
+        //redirect to the route '/feature'
+        //use react router for navigation
+        browserHistory.push('/feature');
+      })
+        .catch(() => {
+          //if request is bad ..
+          //Show an error to the user
+        });
   }
 }
