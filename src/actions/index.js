abceba7ba7ee488690ae347aaf4e64f4d5647503
Srivatsa S <srivatsashankaran@gmail.com>
2017-08-08 16:16:03 +0530

    Finishing the Signup action creator for the use case when the email is already in use

--- src/actions/index.js
+++ src/actions/index.js
@@ -28,6 +28,19 @@ export function signInUser({ email, password }){
   }
 }
 
+export function signupUser({ email, password}) {
+  //again we use redux thunk
+  return function(dispatch) {
+    axios.post(`${ROOT_URL}/signup`, { email, password })
+      .then(response => {
+        dispatch({ type: AUTH_USER });
+        localStorage.setItem('token', response.data.token);
+        browserHistory.push('/feature');
+      })
+        .catch(error => dispatch(authError(error.response.data.error)));
+  }
+}
+
 
 export function authError(error) {
   return {
@@ -43,12 +56,4 @@ export function signoutUser(){
   return{
     type: UNAUTH_USER
   };
-}
-
-
-export function signupUser({ email, password}) {
-  //again we use redux thunk
-  return function(dispatch) {
-    axios.post(`${ROOT_URL}/signup`, { email, password })
-  }
 }
